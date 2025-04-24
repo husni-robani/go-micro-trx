09f732ed20ff078dbb0586fa86f5f194d86fe86e
husni_robani <husnir2005@gmail.com>
2025-04-25 03:15:56 +0700

    add helpers

---
+++ broker-service/cmd/api/helpers.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+type jsonResponse struct {
+	Error bool `json:"error"`
+	Message string `json:"message"`
+	Data any `json:"data,omitempty"`
+}
+
+func (app *Config) writeResponse(w http.ResponseWriter, statusCode int, data any) error {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(statusCode)
+
+	jsonData, err := json.Marshal(data)
+	if err != nil {
+		log.Println("Failed to marshaling data: ", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return err
+	}
+
+	w.Write(jsonData)
+
+	return nil
+}
+
+func (app *Config) errorResponse(w http.ResponseWriter, statusCode int, err error) {
+	var payload jsonResponse
+	payload.Error = true
+	payload.Message = err.Error()
+	
+	app.writeResponse(w, statusCode, payload)
+}
