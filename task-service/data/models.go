e9b98e247e97c071dfaafb29b6b686ed63962e9f
husni_robani <husnir2005@gmail.com>
2025-04-30 02:04:34 +0700

    fix approve and reject task

--- task-service/data/models.go
+++ task-service/data/models.go
@@ -43,13 +43,13 @@ func (t *Task) CreateTask(newTask Task) error {
 	return nil
 }
 
-func (t *Task) ApproveTask(taskId int) error {
+func (t *Task) ApproveTask() error {
 	ctx, cancel := context.WithTimeout(context.Background(), 20 * time.Second)
 	defer cancel()
 
 	query := "UPDATE tasks set status = 1, step = 2 WHERE task_id = $1"
 
-	result, err := db.ExecContext(ctx, query, taskId)
+	result, err := db.ExecContext(ctx, query, t.TaskID)
 	if err != nil {
 		log.Println("Failed to update task to approve: ", err)
 		return err
@@ -61,12 +61,12 @@ func (t *Task) ApproveTask(taskId int) error {
 	return nil
 }
 
-func (t *Task) RejectTask(taskId int) error {
+func (t *Task) RejectTask() error {
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second * 20)
 	defer cancel()
 
 	query := "UPDATE tasks SET status = 2 WHERE task_id = $1"
-	result, err := db.ExecContext(ctx, query, taskId)
+	result, err := db.ExecContext(ctx, query, t.TaskID)
 	if err != nil {
 		log.Println("Failed to reject task: ", err)
 		return err
