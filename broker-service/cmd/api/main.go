3105d304b8acb9369ba2c95ea34f5a207e3b2e44
husni_robani <husnir2005@gmail.com>
2025-05-24 09:22:41 +0700

    add connection to rpc

--- broker-service/cmd/api/main.go
+++ broker-service/cmd/api/main.go
@@ -3,18 +3,24 @@ package main
 import (
 	"log"
 	"net/http"
+	"net/rpc"
+	"time"
 )
 
 const(
 	webPort = "80"
 )
 
-type Config struct{}
+type Config struct{
+	rpcClient *rpc.Client
+}
 
 func main() {
 	log.Println("Running broker-service on port ", webPort)
 
-	app := Config{}
+	app := Config{
+		rpcClient: connectRPC(),
+	}
 
 	srv := &http.Server{
 		Addr: ":" + webPort,
@@ -24,4 +30,26 @@ func main() {
 	if err := srv.ListenAndServe(); err != nil {
 		log.Panic("Failed to run broker-service")
 	}
+}
+
+func connectRPC() *rpc.Client{
+	counter := 1
+
+	for {
+		client, err := rpc.Dial("tcp", "task-service:50001")
+		if err != nil {
+			if counter >= 5 {
+				log.Panic("RPC Connection failed: ", err)
+			}
+			log.Printf("RPC not connected yet: %v (waiting for 2 second)\n", err)
+			time.Sleep(time.Second * 2)
+			counter ++
+			continue
+		}
+
+		log.Println("RPC Connected!")
+		
+		return client
+	}
+
 }
