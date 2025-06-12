89117fdb051c2d7bb45e0416674adee3431b0bf5
husni_robani <husnir2005@gmail.com>
2025-06-12 16:02:12 +0700

    implement gRPC client side for task

--- broker-service/cmd/main.go
+++ broker-service/cmd/main.go
@@ -8,11 +8,17 @@ import (
 	"net/rpc"
 	"os"
 	"time"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+
+	taskpb "proto/task"
 )
 
 func main() {
 	app := config.Config{
 		RPCClientTask: connectRPCClient(os.Getenv("TASK_RPC_ADDRESS")),
+		GRPCClientTask: connectTaskServiceGRPCClient(),
 	}
 
 	api := api.APIHandler{
@@ -44,7 +50,7 @@ func connectRPCClient(address string) *rpc.Client{
 	for {
 		client, err := rpc.Dial("tcp", address)
 		if err != nil {
-			if counter >= 5 {
+			if counter >= 10 {
 				log.Panicf("RPC | %v Connection failed: %v", address, err)
 			}
 			log.Printf("RPC not connected yet: %v (waiting for 2 second)\n", err)
@@ -58,4 +64,29 @@ func connectRPCClient(address string) *rpc.Client{
 		return client
 	}
 
+}
+
+func connectTaskServiceGRPCClient() taskpb.TaskServiceClient {
+	counter := 1
+	task_service_address := os.Getenv("TASK_GRPC_ADDRESS")
+
+	for {	
+		conn, err := grpc.NewClient(task_service_address, grpc.WithTransportCredentials(insecure.NewCredentials()))
+		if err != nil {
+			if counter >= 5 {
+				log.Panicf("gRPC | %v connection failed: %v", task_service_address, err)
+			}
+
+			log.Printf("gRPC not connected yet: %v (waiting for 2 second)\n", err)
+			time.Sleep(time.Second * 2)
+			counter ++
+			continue
+		}
+
+		client := taskpb.NewTaskServiceClient(conn)
+
+		log.Printf("%v Connected!", task_service_address)
+
+		return client
+	}
 }
